91cebc92c836a053cb450ad16bcabd9fbe0310e6
Kalpeshsuthar <kalpesh@webtechsystem.com>
2016-09-23 19:57:13 +0530

    Add files via upload

---
+++ FunctionsToRemoveCachedata.java
@@ -0,0 +1,4 @@
+698:  
+When user 1 (requester) is in chat dialogue screen 
+but user 2 (fashion pro) is outside app and a chat message is sent to user 2, 
+a pink circle notification appears next to user 2 "My Requests" instead of "Other Requests".
